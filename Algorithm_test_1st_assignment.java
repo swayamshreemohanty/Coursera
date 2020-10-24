1e0f88439eaa0c42cac4999e4941a74032f17451
swayamshreemohanty <mohantyswayamshree@gmail.com>
2020-10-25 01:07:54 +0530

    Coursera

---
+++ Algorithm_test_1st_assignment.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Algorithm_test_1st_assignment
+{
+    public static void sequence ()
+    {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter numbers to find the sequence: ");
+        int n= input.nextInt();
+        int multi=3;
+        int length= n+(n+2);
+        int sequence=n*3;
+
+        int [] seq= new int[length];
+        for(int i=2;i<=length+1;i++)
+        {
+            System.out.printf("%d ",sequence);
+            sequence+=2;
+        }
+            System.out.println("\n");
+    }
+    public static void main(String[]args)
+    {
+        sequence();
+    }
+
+}
---
+++ Coursera_Alogirthm_Quiz_Assignment.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Coursera_Alogirthm_Quiz_Assignment 
+{
+    
+    public static void series()
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the number for the sequence:");
+        int N= input.nextInt();
+
+        int variable= -N*N;
+
+        for(int i=1; i<=(3*N);i++)
+        {
+            System.out.printf("%d ",variable);
+            variable=(variable+(2*i))-1;
+        }
+
+        //System.out.println("Result is: "+variable);
+    }
+
+    public static void main(String[]args)
+    {
+        series();
+    }
+}
